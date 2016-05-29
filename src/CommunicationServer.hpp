05ae2ad81f17c41abf9365f54b447dfdbdadd38b
Ilari Jääkeläinen <ilari.jaaskelainen@hotmail.com>
2016-05-29 12:33:30 +0300

    void

--- src/CommunicationServer.hpp
+++ src/CommunicationServer.hpp
@@ -1,6 +1,10 @@
 // CommunicationServer :: Text-based IPC hub.
 #include "GenericNetworking.hpp"
 class CommunicationServer {
+	/*
+	    ProcessDispatcher:
+	     CommunicationConnector bidirectional traffic container, do not expose.
+	*/
 	class ProcessDispatcher {
 		//
 	public:
@@ -13,11 +17,14 @@ private:
 	GenericNetworking *CommunicationHub;
 	// old
 	int		process_dispatcher(void);
-	void	build_select_list(void);
-	void	read_socks(void);
-	void	handle_new_connection(void);
-	void	deal_with_data(int listnum);
-	void	setnonblocking(int sock);
+	void		build_select_list(void);
+	void		read_socks(void);
+	void		handle_new_connection(void);
+	void		deal_with_data(int listnum);
+	void		setnonblocking(int sock);
+	bool		is_paused_fd(int fd);
+	vector<int>	paused_fds;
+	vector<int> stupid_fds;
 	int		__process_dispatcher(void);
 public:
 	CommunicationServer();
